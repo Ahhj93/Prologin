72f0296098687eb672a8d56117b168efbe974c87
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2023-10-28 23:01:15 +0200

    Update

---
+++ P3.py
@@ -0,0 +1,30 @@
+N: int = int(input()) # Nombre de villes
+M: int = int(input()) # Nombre d'années avant le Ragnarök
+villes: list = [] # Liste des villes
+for i in range(N):
+    villes.append(input())
+actions: list = [elt for elt in input()] # Liste des actions
+
+ventre: list = [] # Liste des villes mangées
+indice_ventre: int = 0 # Indice de la ville mangée actuelle
+
+for action in actions:
+    if action == "A":
+        # Avancer d'une ville de manière circulaire dans la liste
+        villes = villes[1:] + villes[:1]
+    elif action == "M":
+        # Manger la ville la plus proche devant sa tête
+        ventre.append(villes.pop(0))
+    elif action == "R":
+        # Se retourner, ce qui signifie qu'il avancera alors dans
+        # la direction opposée par la suite. S'il avançait de gauche à droite,
+        # il avance alors de droite à gauche, et inversement
+        villes = villes[::-1]
+    elif action == "C":
+        # Recracher la dernière ville qu'il a mangé et qu'il n'a pas encore recraché,
+        # pour la placer devant sa tête
+        villes.insert(0, ventre.pop(len(ventre)-1))
+
+# On affiche les villes
+for ville in villes:
+    print(ville)
--- perf2_unPeuDOrdre.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-k = randint(1, 1000)
-n = randint(1, 100_000)
-print(k)
-print(n)
-for i in range(n):
-    print(randint(1, 1000), end=" " if i < n - 1 else "\n")
